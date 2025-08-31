71be7eb1abd610a5cdcfb8ceda11a6abd3ece572
Xhispas <acevedo.candela066@gmail.com>
2025-08-31 20:34:10 -0300

    cositas (esperen loco)
    si cosito ya te vimos

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -1,6 +1,7 @@
 package ejercicio1;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Empleado extends Persona{
 	
@@ -32,7 +33,41 @@ public class Empleado extends Persona{
 		return super.toString() + ", Legajo = " + legajo + ", Puesto = " + puesto;
 	}
 	
-    public static int getProximoLegajo() {
+	public int compareTo(Empleado o) {
+		
+		if(o.legajo == this.legajo) return 0;
+			
+			if(o.legajo<this.legajo) { return -1;} return 1;}
+	
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + legajo;
+		result = prime * result + ((puesto == null) ? 0 : puesto.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empleado other = (Empleado) obj;
+		if (legajo != other.legajo)
+			return false;
+		if (puesto == null) {
+			if (other.puesto != null)
+				return false;
+		} else if (!puesto.equals(other.puesto))
+			return false;
+		return true;
+	}
+
+	public static int getProximoLegajo() {
         return cont + 1;
     }
 	
@@ -46,4 +81,8 @@ public class Empleado extends Persona{
 	public void setPuesto(String puesto) {
 		this.puesto = puesto;
 	}
+
+	
+	
+	
 }

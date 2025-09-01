18ff67050ce9fd0d1539fe9f26f4adfe47ed1b39
QuinnnDev <joacodvecchia@gmail.com>
2025-08-31 21:31:04 -0300

    added implement in empleado.java
    Cosito fuiste degradado a brazuca

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -3,7 +3,7 @@ package ejercicio1;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class Empleado extends Persona{
+public class Empleado extends Persona implements Comparable<Empleado>{
 	
 	//Atributos
 	private final int legajo;
@@ -33,11 +33,17 @@ public class Empleado extends Persona{
 		return super.toString() + ", Legajo = " + legajo + ", Puesto = " + puesto;
 	}
 	
+    @Override
 	public int compareTo(Empleado o) {
 		
 		if(o.legajo == this.legajo) return 0;
 			
-			if(o.legajo<this.legajo) { return -1;} return 1;}
+		if(o.legajo<this.legajo) 
+		{ 
+			return -1;
+		}
+		
+		return 1;}
 	
     @Override
 	public int hashCode() {

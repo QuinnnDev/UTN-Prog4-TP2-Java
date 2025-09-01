cf24cc20d4b7c18e2a8bb7951c32dda96f7024c9
Keys4Ever <cubillagonzalo@outlook.com>
2025-09-01 19:59:31 -0300

    El último nadie lo leyó, así que supongo que puedo seguir contando en los mensajes de los commits que antes de hacer este me hice una pajita, tipo, la empecé antes de empezar a hacer esto y me acordé a media paja q tenía que aportar, así que seguí haciendola mientras con la otra aportaba. No voy  a decir a quien pero a alguien del grupo se la dediqué

--- src/ejercicio1/MainEjercicio1_b.java
+++ src/ejercicio1/MainEjercicio1_b.java
@@ -8,9 +8,9 @@ public class MainEjercicio1_b {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Empleado p1 = new Empleado("12345678", "Joaquin", "Devec", 		LocalDate.of(2005, 10, 11), "masculino", "Puerto madero", "22222222", "asdf1@gmail.com", "Contador");
+		Empleado p1 = new Empleado("12345678", "Joaquin", "Devec", 		LocalDate.of(2005, 10, 11), "Femboy", "Femboy island", "22222222", "asdf1@gmail.com", "Esposo de ruso");
 		Empleado p2 = new Empleado("12345679", "Juan", "Cruz",			LocalDate.of(2005, 6, 15), "masculino", "Santa Fe 4321", "1111111111", "asdf2@gmail.com", "contador");
-		Empleado p3 = new Empleado("12345680", "Gonzalo", "Cubilla",	LocalDate.of(2004, 12, 13), "paraguayo", "Zimbabwe 5421", "33333333", "asdf3@gmail.com", "programador");
+		Empleado p3 = new Empleado("12345680", "Gonzalo", "Cubilla",	LocalDate.of(2004, 12, 13), "masculino", "Argentina 5421", "33333333", "asdf3@gmail.com", "programador");
 		Empleado p4 = new Empleado("12345681", "Jorge", "El Curioso", 	LocalDate.of(2006, 10, 11), "masculino", "Belgrano 124", "44444444", "asdf4@gmail.com", "jefe");
 		Empleado p5 = new Empleado("12345682", "Leandro", "Paredes", 	LocalDate.of(1997,12,11), "masculino", "Formosa 1111", "55555555", "asdf5@gmail.com", "seguridad");
 		
--- src/ejercicio1/MainEjercicio1_d.java
+++ src/ejercicio1/MainEjercicio1_d.java
@@ -10,9 +10,9 @@ public class MainEjercicio1_d {
 		// TODO Auto-generated method stub
 
 		
-		Persona p1 = new Persona("12345678", "Joaquin", "Devec", 		LocalDate.of(2005, 10, 11), "masculino", "Puerto madero", "22222222", "asdf1@gmail.com");
+		Persona p1 = new Persona("12345678", "Joaquin", "Devec", 		LocalDate.of(2005, 10, 11), "femboy", "Femboy island", "22222222", "asdf1@gmail.com");
 		Persona p2 = new Persona("12345679", "Juan", "Cruz",			LocalDate.of(2005, 6, 15), "masculino", "Santa Fe 4321", "1111111111", "asdf2@gmail.com");
-		Persona p3 = new Persona("12345680", "Gonzalo", "Cubilla",	LocalDate.of(2004, 12, 13), "masculino", "Zimbabwe", "33333333", "asdf3@gmail.com");
+		Persona p3 = new Persona("12345680", "Gonzalo", "Cubilla",	LocalDate.of(2004, 12, 13), "masculino", "Argentina", "33333333", "asdf3@gmail.com");
 		Persona p4 = new Persona("12345681", "Jorge", "El Curioso", 	LocalDate.of(2006, 10, 11), "masculino", "Belgrano 124", "44444444", "asdf4@gmail.com");
 		Persona p5 = new Persona("12345682", "Leandro", "Paredes", 	LocalDate.of(1997,12,11), "masculino", "Formosa 1111", "55555555", "asdf5@gmail.com");
 		
@@ -24,7 +24,11 @@ public class MainEjercicio1_d {
 		set.add(p4);
 		set.add(p5);
 		
+		Iterator<Persona> it = set.iterator();
 		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
 		
 	}
 

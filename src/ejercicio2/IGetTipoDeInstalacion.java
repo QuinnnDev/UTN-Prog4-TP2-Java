211a90dde6fc7599549bf54bd7d6d171e7a94c6a
QuinnnDev <joacodvecchia@gmail.com>
2025-09-02 00:33:58 -0300

    Aporte 1 ejercicio 2

---
+++ src/ejercicio2/IGetTipoDeInstalacion.java
@@ -0,0 +1,13 @@
+package ejercicio2;
+
+public interface IGetTipoDeInstalacion {
+
+	/*
+	 * La funcion devuelve un int en funcion de que tipo 
+	 * de instalacion sea la instalacion en la cual se ejecuta 
+	 * 
+	 */
+	
+	public int getTipoDeInstalacion();
+	
+}
---
+++ src/ejercicio2/MainEjercicio2.java
@@ -0,0 +1,15 @@
+package ejercicio2;
+
+public class MainEjercicio2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("texto para ver si funcion, borrenlo cuando haya algo mas xd");
+		
+		
+		
+		
+	}
+
+}

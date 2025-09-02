f9200055fe289cf068cc357026967017a93a5a39
Keys4Ever <cubillagonzalo@outlook.com>
2025-09-02 13:29:52 -0300

    Volvieron a leer los mensajes de los commits, matense, ahora voy a encriptar lo que digo: Nf ijdf vob qbkb dpnp ef dptuvncsf njfousbt ibdíb ftuf dpnnju, qfsp ftub wfa gvf ftqfdjbmnfouf sáqjeb, vopt 15 njovupt, qpsrvf tf mb efejrvé b kjkjkj

---
+++ src/ejercicio2/InstalacionDeportiva.java
@@ -0,0 +1,10 @@
+package ejercicio2;
+
+public class InstalacionDeportiva {
+    private int tipoDeInstalacion;
+
+    public InstalacionDeportiva(int tipoDeInstalacion) {
+        this.tipoDeInstalacion = tipoDeInstalacion;
+    }
+ 
+}

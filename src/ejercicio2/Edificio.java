571a930547e16e6e8847a61251768c8397a3ecb4
Xhispas <acevedo.candela066@gmail.com>
2025-09-02 21:15:42 -0300

    cree la clase edificio porque ella es fa
    natica de lo sensual, ella tiene una foto mia, y Y ya me la puedo imaginar
    Lo que hace cuando esta sólita
    Pero yo le voy a preguntar
    Y a escuchar su voz cuando se agita
    Por su manera de respirar
    Puedo imaginarme lo que estas haciendo
    Si le hablo malo se pone intranquila
    Pasa su mano por todo su cuerpo
    Cuando le digo todo lo que le haría
    Puedo imaginarme lo que esta haciendo
    Y eso que solo es una foto mía
    Que pasara cuando nos encontremos
    De seguro que se lo explicaría
    Cada vez que hablamos me dice que quieres verme
    Loca por conocerme, solo piensa en ese día
    Ver mi fotografía es lo que le daña la mente
    Con el cuerpo que tiene, dime quien no le daría
    Llama de madrugada, tan caliente como siempre
    Me habla de una manera, que ni yo me atrevería
    Veremos lo que pasa cuando me tenga de frente
    Es que al igual que ella, también pienso en ese día
    Es fanática de lo sensual
    Ella tiene una foto mía
    Y ya me la puedo imaginar
    Lo que hace cuando esta sólita.

---
+++ src/ejercicio2/Edificio.java
@@ -0,0 +1,21 @@
+package ejercicio2;
+
+public class Edificio {
+
+	 private Double superficie;
+	 
+
+	public Edificio(Double superficie) {
+		this.superficie = superficie;
+	}
+
+	public Double getSuperficie() {
+		return superficie;
+	}
+
+	public void setSuperficie(Double superficie) {
+		this.superficie = superficie;
+	}
+	 
+	
+}

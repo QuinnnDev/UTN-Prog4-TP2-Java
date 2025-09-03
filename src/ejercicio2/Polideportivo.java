34a472232f54c9d1b30e5cbf02f8339af2c5c347
Xhispas <acevedo.candela066@gmail.com>
2025-09-02 21:34:36 -0300

    chicos basta me hace mal que...
    toy Chica sea la unica animatronico que lleva ropa la cual es un short, que para mi la hace ver muy sexy y los hombres la adoran. A Toy Chicalos hombres le han hecho muchas fotos en versiones muy sexys, y esta claro porque lo han hecho, es la favorita de muchos hombres. Por la mayoria de los hombres

---
+++ src/ejercicio2/Polideportivo.java
@@ -0,0 +1,30 @@
+package ejercicio2;
+
+public class Polideportivo {
+	
+	private double superficie;
+	private String nombre;
+	
+	
+	public Polideportivo(double superficie, String nombre) {
+	
+		this.superficie = superficie;
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public double getSuperficie() {
+		return superficie;
+	}
+	public void setSuperficie(double superficie) {
+		this.superficie = superficie;
+	}
+	
+	
+
+}

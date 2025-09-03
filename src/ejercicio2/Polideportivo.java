608364d8c2da647f63b7773108d626a1efbc6514
Keys4Ever <cubillagonzalo@outlook.com>
2025-09-02 22:04:49 -0300

    Nadie vio todavía el mensaje de commit anterior, como personas normales, ojalá se mantenga así por mas tiempo y nadie descubra lo q escribí ahí

--- src/ejercicio2/Polideportivo.java
+++ src/ejercicio2/Polideportivo.java
@@ -1,15 +1,16 @@
 package ejercicio2;
 
-public class Polideportivo {
+public class Polideportivo implements IGetTipoDeInstalacion, IGetSuperficieEdificio{
 	
 	private double superficie;
 	private String nombre;
+	private int tipoInstalacion;
 	
 	
-	public Polideportivo(double superficie, String nombre) {
-	
+	public Polideportivo(double superficie, String nombre, int tipoDeInstalacion) {
 		this.superficie = superficie;
 		this.nombre = nombre;
+		this.tipoInstalacion = tipoDeInstalacion;
 	}
 	
 	public String getNombre() {
@@ -18,13 +19,22 @@ public class Polideportivo {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	public double getSuperficie() {
+	
+	public double getSuperficieEdificio() {
 		return superficie;
 	}
+
+	public int getTipoDeInstalacion() {
+		return tipoInstalacion;
+	}
+	
+	public void setTipoDeInstalacion(int tipo) {
+		this.tipoInstalacion = tipo;
+	}
+	
 	public void setSuperficie(double superficie) {
 		this.superficie = superficie;
 	}
-	
 	
 
 }
